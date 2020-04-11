549368f5243609c4e14ab7be9014a0bb72d6e245
erika fernanda <ericka_nanda@hotmail.com>
2020-04-11 10:56:53 -0400

    Editar Empleado
    Se realizo el editar del formulario empleado

--- application/controllers/Mantenimiento/Empleado.php
+++ application/controllers/Mantenimiento/Empleado.php
@@ -58,4 +58,75 @@ class Empleado extends BaseController
             $this->index();
         }
     }
+    public function editar($id_empleados)
+    {
+        $data = array(
+            'Empleado' => $this->Empleado_model->getEmpleado($id_empleados),
+        );
+
+        $this->loadView('Empleados', '/form/admin/empleado/editar', $data);
+    }
+    public function actualizarEmpleado()
+    {
+        $id_empleados = $this->input->post('id_empleados');
+        $nombres = $this->input->post('nombre');
+        $apellidos = $this->input->post('apellidos');
+        $telefono1 = $this->input->post('telefono1');
+        $telefono2 = $this->input->post('telefono2');
+        $direccion = $this->input->post('direccion');
+        $num_documento = $this->input->post('num_documento');
+        $tipo_documento = $this->input->post('tipo_documento');
+
+        $empleado_actual = $this->Empleado_model->getEmpleado($id_empleados);
+
+        $this->form_validation->set_rules("nombre", "Nombre", "required");
+        $this->form_validation->set_rules("apellidos", "Apellidos", "required");
+        if ($num_documento == $empleado_actual->num_documento) {
+        } else {
+            $this->form_validation->set_rules(
+                'num_documento',
+                'num_documento',
+                array(
+                    'required',
+                    array('validarCi', array($this->Empleado_model, 'validarCi'))
+                ),
+                array('validarCi' => 'Carnet de identidad ya esta siendo ocupado')
+            );
+        }
+       
+        $this->form_validation->set_rules("telefono1", "telefono1", "required");       
+        $this->form_validation->set_rules("direccion", "direccion", "required");      
+        $this->form_validation->set_rules('tipo_documento', 'tipo_documento', 'trim|required');
+
+        if ($this->form_validation->run()) {
+            $datos= array(
+                'nombre' => $nombres,
+                'apellidos' => $apellidos,
+                'telefono_01' => $telefono1,
+                'telefono_02' => $telefono2,
+                'direccion' => $direccion,   
+                'num_documento' => $num_documento,
+                'estado' => '1',
+            );
+            
+            if ($this->Empleado_model->actualizar($id_empleados, $tipo_documento, $datos)) {
+                redirect(base_url() . "Mantenimiento/Empleado");
+
+            } else {
+                $this->session->set_flashdata("error", "No se pudo actualizar la informacion");
+                redirect(base_url() . "/form/admin/empleado/editar" . $id_empleados);
+            }
+        } else {
+            $this->editar($id_empleados);
+        }
+    }
+    public function borrar($id_usuarios)
+    {
+        $datos = array(
+            'estado' => '0' ,
+            'fecha_salida' => date('Y-m-d') ,
+     );
+        $this->Usuario_model->borrar($id_usuarios, $datos);
+        echo 'Formularios/Usuarios';
+    }
 }
--- application/models/Empleado_model.php
+++ application/models/Empleado_model.php
@@ -41,5 +41,34 @@ class Empleado_model extends CI_Model
 
         return $this->db->insert('empleados', $datosEmpleado);
     }
+    public function getEmpleado($id_empleados)
+    {
+        $this->db->select('e.*, td.nombre as tipo_documento');
+        $this->db->from('empleados e');
+        $this->db->join('tipo_documento td', 'td.id_tipo_documento = e.id_tipo_documento');
+        $this->db->where('e.estado', '1');
+        $this->db->where('e.id_empleados', $id_empleados);
+        $resultado = $this->db->get()->row();
+
+        if (isset($resultado)) {
+            return $resultado;
+        } else {
+            return false;
+        }
+    }
+    public function actualizar($id_empleados, $tipo_documento, $datos)
+    {
+        $this->db->where('nombre', $tipo_documento);
+        $tipo_documento = $this->db->get('tipo_documento')->row();
+        $datos['id_tipo_documento'] = $tipo_documento->id_tipo_documento;
+
+        $this->db->where('id_empleados', $id_empleados);
+        return $this->db->update('empleados', $datos);
+    }
+    public function borrar($id_usuarios, $datos)
+    {
+        $this->db->where('id_usuarios', $id_usuarios);
+        return $this->db->update('usuarios', $datos);
+    }
     
 }
