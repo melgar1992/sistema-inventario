b0b48c180d07ad4c9a6c8f04bfeee81fa3111945
erika fernanda <ericka_nanda@hotmail.com>
2020-07-27 10:17:49 -0400

    Formulario salida de vestuario
    Se arreglo los nombres

--- application/views/form/admin/ventas/editar.php
+++ application/views/form/admin/ventas/editar.php
@@ -3,7 +3,7 @@
      <div class="">
          <div class="page-title">
              <div class="title_left">
-                 <h3>Formulario Salida Vestuario</h3>
+                 <h3>Editar Salida de Vestuario</h3>
              </div>
 
              <div class="title_right">
@@ -16,7 +16,7 @@
              <div class="col-md-12 col-sm-12 col-xs-12">
                  <div class="x_panel">
                      <div class="x_title">
-                         <h2>Editar Salida de Vestuario</h2>
+                         <h2>Salida de Vestuario</h2>
                          <ul class="nav navbar-right panel_toolbox">
                              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                              </li>
